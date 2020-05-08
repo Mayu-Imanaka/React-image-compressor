2b5f8d27ede8ddf1b192ea148662159e682ae8b9
Mayu Imanaka <imamayu.1215@gmail.com>
2020-05-08 12:00:40 -0700

    Updates

---
+++ precache-manifest.fb99efb8eae38a61b1bc5ce23bdca1c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4b03117249b2a94a5638f1dd8018c1",
+    "url": "/React-image-compressor/index.html"
+  },
+  {
+    "revision": "3106b06681c742e0152b",
+    "url": "/React-image-compressor/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "08b28886346b1cc3e32c",
+    "url": "/React-image-compressor/static/css/main.c7c44ed2.chunk.css"
+  },
+  {
+    "revision": "3106b06681c742e0152b",
+    "url": "/React-image-compressor/static/js/2.9f1f7397.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-image-compressor/static/js/2.9f1f7397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08b28886346b1cc3e32c",
+    "url": "/React-image-compressor/static/js/main.844216b3.chunk.js"
+  },
+  {
+    "revision": "6e6db5acc8de7aead050",
+    "url": "/React-image-compressor/static/js/runtime-main.29988f90.js"
+  }
+]);
